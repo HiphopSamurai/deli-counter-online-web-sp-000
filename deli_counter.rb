f47aea5eb736d6925bafdf745f515db43489c394
Garrett Thompson <gtcruz.gt@gmail.com>
2019-07-13 05:22:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(array)
   number = 1
   if array.length == 0
-    puts "The line is currently empty"
+    puts "The line is empty"
   else
     puts "The line is currently: "
     array.each do |name|
